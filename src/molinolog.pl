81e46dee77d20aca646705417a0d66a9479fc35a
nico-rodriguez <nico-rodriguez@users.noreply.github.com>
2019-05-27 00:13:08 -0300

    Fix error en molino.

--- src/molinolog.pl
+++ src/molinolog.pl
@@ -98,50 +98,58 @@ chequear_molino(Dir,Dist,Visual,Turno) :-
             (member(ficha(Turno,Dir,Dist),[ficha(Turno,Dir1,Dist1),ficha(Turno,Dir2,Dist2),ficha(Turno,Dir3,Dist3)]),molino(ficha(Turno,Dir1,Dist1),ficha(Turno,Dir2,Dist2),ficha(Turno,Dir3,Dist3))),
             MolinosNuevos),
     length(MolinosNuevos,N),
-    N > 0.
+    (   N > 0
+    ->  capturar_ficha(Visual,Turno,JugadorNegro,JugadorBlanco,T)
+    ;   true
+    ).
 
 % Molinos horizontales
 molino(ficha(Turno,nw,N),ficha(Turno,n,N),ficha(Turno,ne,N)) :-
-    clause(clause(ficha(Turno,nw,N),true)),
-    clause(clause(ficha(Turno,n,N),true)),
-    clause(clause(ficha(Turno,ne,N),true)),
+    clause(ficha(Turno,nw,N),true),
+    clause(ficha(Turno,n,N),true),
+    clause(ficha(Turno,ne,N),true),
     !.
 molino(ficha(Turno,w,3),ficha(Turno,w,2),ficha(Turno,w,1)) :-
-    clause(clause(ficha(Turno,w,3),true)),
-    clause(clause(ficha(Turno,w,2),true)),
-    clause(clause(ficha(Turno,w,1),true)),
+    clause(ficha(Turno,w,3),true),
+    clause(ficha(Turno,w,2),true),
+    clause(ficha(Turno,w,1),true),
     !.
 molino(ficha(Turno,e,1),ficha(Turno,e,2),ficha(Turno,e,3)) :-
-    clause(clause(ficha(Turno,e,1),true)),
-    clause(clause(ficha(Turno,e,2),true)),
-    clause(clause(ficha(Turno,e,3),true)),
+    clause(ficha(Turno,e,1),true),
+    clause(ficha(Turno,e,2),true),
+    clause(ficha(Turno,e,3),true),
     !.
 molino(ficha(Turno,sw,N),ficha(Turno,s,N),ficha(Turno,se,N)) :-
-    clause(clause(ficha(Turno,sw,N),true)),
-    clause(clause(ficha(Turno,s,N),true)),
-    clause(clause(ficha(Turno,se,N),true)),
+    clause(ficha(Turno,sw,N),true),
+    clause(ficha(Turno,s,N),true),
+    clause(ficha(Turno,se,N),true),
     !.
 % Molinos verticales
 molino(ficha(Turno,nw,N),ficha(Turno,w,N),ficha(Turno,sw,N)) :-
-    clause(clause(ficha(Turno,nw,N),true)),
-    clause(clause(ficha(Turno,w,N),true)),
-    clause(clause(ficha(Turno,sw,N),true)),
+    clause(ficha(Turno,nw,N),true),
+    clause(ficha(Turno,w,N),true),
+    clause(ficha(Turno,sw,N),true),
     !.
 molino(ficha(Turno,n,3),ficha(Turno,n,2),ficha(Turno,n,1)) :-
-    clause(clause(ficha(Turno,n,3),true)),
-    clause(clause(ficha(Turno,n,2),true)),
-    clause(clause(ficha(Turno,n,1),true)),
+    clause(ficha(Turno,n,3),true),
+    clause(ficha(Turno,n,2),true),
+    clause(ficha(Turno,n,1),true),
     !.
 molino(ficha(Turno,s,1),ficha(Turno,s,2),ficha(Turno,s,3)) :-
-    clause(clause(ficha(Turno,s,1),true)),
-    clause(clause(ficha(Turno,s,2),true)),
-    clause(clause(ficha(Turno,s,3),true)),
+    clause(ficha(Turno,s,1),true),
+    clause(ficha(Turno,s,2),true),
+    clause(ficha(Turno,s,3),true),
     !.
 molino(ficha(Turno,ne,N),ficha(Turno,e,N),ficha(Turno,se,N)) :-
-    clause(clause(ficha(Turno,ne,N),true)),
-    clause(clause(ficha(Turno,e,N),true)),
-    clause(clause(ficha(Turno,se,N),true)),
+    clause(ficha(Turno,ne,N),true),
+    clause(ficha(Turno,e,N),true),
+    clause(ficha(Turno,se,N),true),
     !.
+
+% capturar_ficha(Visual,Turno,JugadorNegro,JugadorBlanco,T) :-
+%     sformat(Msg, 'Jugador ~w, capturar', [Turno]),
+%     gr_estado(Visual, Msg),
+
 
 posicion_libre(Dir,Dist) :-
     \+ clause(ficha(_,Dir,Dist),true).
